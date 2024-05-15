35c019d607c8f0cdc43def22afe2f878fc046fd0
Luan <158794560+Nemoske@users.noreply.github.com>
2024-05-14 22:28:34 -0300

    TestandoWhile.java

--- src/PraticasSemPoo/TestandoWhile.java
+++ src/PraticasSemPoo/TestandoWhile.java
@@ -1,19 +1,18 @@
 package PraticasSemPoo;
 
 public class TestandoWhile {
-    public static void main(String[] args) {
-        int recebe = 0;
-        boolean teste = true;
-        while(teste){
-            int contador = 0;
-            contador++;
-            recebe += contador;
-            System.out.println(recebe);
-            if(contador <= 10) {
-                teste = false;
-            }
-        }
-        System.out.println(recebe);
-
+  public static void main(String[] args) {
+    int recebe = 0;
+    int contador = 0;
+    boolean teste = true;
+    while (teste) {
+      contador++;
+      recebe += contador;
+      System.out.println(recebe);
+      if (contador >= 10) {
+        teste = false;
+      }
     }
+    System.out.println(recebe);
+  }
 }
